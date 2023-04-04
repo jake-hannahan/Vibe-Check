728fa808b300d34606dd066db4e79bacb86a8deb
Shaun <sragasa97@gmail.com>
2023-04-04 16:47:23 -0400

    TEAM: Installed TailwindCSS

--- ghi/src/Construct.js
+++ ghi/src/Construct.js
@@ -1,18 +1,24 @@
 function Construct(props) {
+	const pad2 = (num) => String(num).padStart(2, "0");
 
-    const pad2 = num => String(num).padStart(2, '0');
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Under construction</h1>
-                <h2>Coming on (or before)</h2>
-                <h2>{props.info.year}-{pad2(props.info.month)}-{pad2(props.info.day)}</h2>
-                <h2>by or <strong>WELL BEFORE</strong> {pad2(props.info.hour)}:{pad2(props.info.min)}</h2>
-                
-            </header>
-        </div>
-    )
+	return (
+		<>
+			<div>
+				<div className="App">
+					<header className="App-header">
+						<h1 className="text-8xl font-bold underline">Under construction</h1>
+						<h2>Coming on (or before)</h2>
+						<h2>
+							{props.info.year}-{pad2(props.info.month)}-{pad2(props.info.day)}
+						</h2>
+						<h2>
+							by or <strong>WELL BEFORE</strong> {pad2(props.info.hour)}:{pad2(props.info.min)}
+						</h2>
+					</header>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Construct;
