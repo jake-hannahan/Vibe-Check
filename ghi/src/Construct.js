2c3424f5d5f5aa7509f9b034efa7b967717628b9
Shaun <sragasa97@gmail.com>
2023-04-04 00:16:42 -0400

    Playing around with Tailwind and general CSS

--- ghi/src/Construct.js
+++ ghi/src/Construct.js
@@ -1,18 +1,295 @@
 function Construct(props) {
+	const pad2 = (num) => String(num).padStart(2, "0");
 
-    const pad2 = num => String(num).padStart(2, '0');
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Under construction</h1>
-                <h2>Coming on (or before)</h2>
-                <h2>{props.info.year}-{pad2(props.info.month)}-{pad2(props.info.day)}</h2>
-                <h2>by or <strong>WELL BEFORE</strong> {pad2(props.info.hour)}:{pad2(props.info.min)}</h2>
-                
-            </header>
-        </div>
-    )
+	return (
+		<>
+			<div className="App">
+				<header className="App-header">
+					<h2 className="text-3xl font-bold mb-2 text-white first">COMING SOON ٩(ˊᗜˋ*)و ♡</h2>
+					<span className="grid grid-cols 2 gap-3 my-4">
+						<h2 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-8xl text-white font-bold first">
+							VIBE CHECK
+						</h2>
+					</span>
+					<h2 className="text-2xl mb-8 text-gray-200 first">April 28th , 2023</h2>
+					<h2 className="text-2xl mb-8 text-gray-200 shake first">Jake Hannahan | Shaun Ragasa</h2>
+					<h2 className="text-2xl mb-8 text-gray-200 shake second">Jay Shastri | Alana Torrez</h2>
+				</header>
+			</div>
+			<div class="bg-white mx-auto max-w-5xl shadow-lg rounded-lg overflow-hidden grid grid-cols-3 gap-3">
+				<div class="sm:flex sm:items-center px-6 py-4">
+					<img
+						class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+						src="https://media2.giphy.com/media/jpbnoe3UIa8TU8LM13/giphy.gif?cid=ecf05e47yb6fo8f80hiw6kmfld0d879i8nyuzb3h5lyg3p5p&rid=giphy.gif&ct=g"
+						alt=""
+					/>
+					<div class="text-center sm:text-left sm:flex-grow">
+						<div class="mb-4">
+							<p class="text-xl leading-tight">SURPRISE</p>
+							<p class="text-sm leading-tight text-gray-400">Bet ya didn't expect that.</p>
+						</div>
+						<div>
+							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border-2 border-purple-400 text-purple-500 hover:bg-purple-600 hover:text-white">
+								Vibes
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="sm:flex sm:items-center px-6 py-4">
+					<img
+						class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+						src="https://media2.giphy.com/media/jpbnoe3UIa8TU8LM13/giphy.gif?cid=ecf05e47yb6fo8f80hiw6kmfld0d879i8nyuzb3h5lyg3p5p&rid=giphy.gif&ct=g"
+						alt=""
+					/>
+					<div class="text-center sm:text-left sm:flex-grow">
+						<div class="mb-4">
+							<p class="text-xl leading-tight">CAT</p>
+							<p class="text-sm leading-tight text-gray-400">You just got catjammed.</p>
+						</div>
+						<div>
+							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border-2 border-purple-400 text-purple-500 hover:bg-purple-600 hover:text-white">
+								All
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="sm:flex sm:items-center px-6 py-4">
+					<img
+						class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+						src="https://media2.giphy.com/media/jpbnoe3UIa8TU8LM13/giphy.gif?cid=ecf05e47yb6fo8f80hiw6kmfld0d879i8nyuzb3h5lyg3p5p&rid=giphy.gif&ct=g"
+						alt=""
+					/>
+					<div class="text-center sm:text-left sm:flex-grow">
+						<div class="mb-4">
+							<p class="text-xl leading-tight">JAM</p>
+							<p class="text-sm leading-tight text-gray-400">Vibe with me.</p>
+						</div>
+						<div>
+							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border-2 border-purple-400 text-purple-500 hover:bg-purple-600 hover:text-white">
+								Around
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="sm:flex sm:items-center px-6 py-4">
+					<img
+						class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+						src="https://media2.giphy.com/media/jpbnoe3UIa8TU8LM13/giphy.gif?cid=ecf05e47yb6fo8f80hiw6kmfld0d879i8nyuzb3h5lyg3p5p&rid=giphy.gif&ct=g"
+						alt=""
+					/>
+					<div class="text-center sm:text-left sm:flex-grow">
+						<div class="mb-4">
+							<p class="text-xl leading-tight">SURPRISE</p>
+							<p class="text-sm leading-tight text-gray-400">Bet ya didn't expect that.</p>
+						</div>
+						<div>
+							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border-2 border-purple-400 text-purple-500 hover:bg-purple-600 hover:text-white">
+								Vibes
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="sm:flex sm:items-center px-6 py-4">
+					<img
+						class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+						src="https://media2.giphy.com/media/jpbnoe3UIa8TU8LM13/giphy.gif?cid=ecf05e47yb6fo8f80hiw6kmfld0d879i8nyuzb3h5lyg3p5p&rid=giphy.gif&ct=g"
+						alt=""
+					/>
+					<div class="text-center sm:text-left sm:flex-grow">
+						<div class="mb-4">
+							<p class="text-xl leading-tight">CAT</p>
+							<p class="text-sm leading-tight text-gray-400">You just got catjammed.</p>
+						</div>
+						<div>
+							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border-2 border-purple-400 text-purple-500 hover:bg-purple-600 hover:text-white">
+								All
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="sm:flex sm:items-center px-6 py-4">
+					<img
+						class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+						src="https://media2.giphy.com/media/jpbnoe3UIa8TU8LM13/giphy.gif?cid=ecf05e47yb6fo8f80hiw6kmfld0d879i8nyuzb3h5lyg3p5p&rid=giphy.gif&ct=g"
+						alt=""
+					/>
+					<div class="text-center sm:text-left sm:flex-grow">
+						<div class="mb-4">
+							<p class="text-xl leading-tight">JAM</p>
+							<p class="text-sm leading-tight text-gray-400">Vibe with me.</p>
+						</div>
+						<div>
+							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border-2 border-purple-400 text-purple-500 hover:bg-purple-600 hover:text-white">
+								Around
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="sm:flex sm:items-center px-6 py-4">
+					<img
+						class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+						src="https://media2.giphy.com/media/jpbnoe3UIa8TU8LM13/giphy.gif?cid=ecf05e47yb6fo8f80hiw6kmfld0d879i8nyuzb3h5lyg3p5p&rid=giphy.gif&ct=g"
+						alt=""
+					/>
+					<div class="text-center sm:text-left sm:flex-grow">
+						<div class="mb-4">
+							<p class="text-xl leading-tight">SURPRISE</p>
+							<p class="text-sm leading-tight text-gray-400">Bet ya didn't expect that.</p>
+						</div>
+						<div>
+							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border-2 border-purple-400 text-purple-500 hover:bg-purple-600 hover:text-white">
+								Vibes
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="sm:flex sm:items-center px-6 py-4">
+					<img
+						class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+						src="https://media2.giphy.com/media/jpbnoe3UIa8TU8LM13/giphy.gif?cid=ecf05e47yb6fo8f80hiw6kmfld0d879i8nyuzb3h5lyg3p5p&rid=giphy.gif&ct=g"
+						alt=""
+					/>
+					<div class="text-center sm:text-left sm:flex-grow">
+						<div class="mb-4">
+							<p class="text-xl leading-tight">CAT</p>
+							<p class="text-sm leading-tight text-gray-400">You just got catjammed.</p>
+						</div>
+						<div>
+							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border-2 border-purple-400 text-purple-500 hover:bg-purple-600 hover:text-white">
+								All
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="sm:flex sm:items-center px-6 py-4">
+					<img
+						class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+						src="https://media2.giphy.com/media/jpbnoe3UIa8TU8LM13/giphy.gif?cid=ecf05e47yb6fo8f80hiw6kmfld0d879i8nyuzb3h5lyg3p5p&rid=giphy.gif&ct=g"
+						alt=""
+					/>
+					<div class="text-center sm:text-left sm:flex-grow">
+						<div class="mb-4">
+							<p class="text-xl leading-tight">JAM</p>
+							<p class="text-sm leading-tight text-gray-400">Vibe with me.</p>
+						</div>
+						<div>
+							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border-2 border-purple-400 text-purple-500 hover:bg-purple-600 hover:text-white">
+								Around
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="sm:flex sm:items-center px-6 py-4">
+					<img
+						class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+						src="https://media2.giphy.com/media/jpbnoe3UIa8TU8LM13/giphy.gif?cid=ecf05e47yb6fo8f80hiw6kmfld0d879i8nyuzb3h5lyg3p5p&rid=giphy.gif&ct=g"
+						alt=""
+					/>
+					<div class="text-center sm:text-left sm:flex-grow">
+						<div class="mb-4">
+							<p class="text-xl leading-tight">SURPRISE</p>
+							<p class="text-sm leading-tight text-gray-400">Bet ya didn't expect that.</p>
+						</div>
+						<div>
+							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border-2 border-purple-400 text-purple-500 hover:bg-purple-600 hover:text-white">
+								Vibes
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="sm:flex sm:items-center px-6 py-4">
+					<img
+						class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+						src="https://media2.giphy.com/media/jpbnoe3UIa8TU8LM13/giphy.gif?cid=ecf05e47yb6fo8f80hiw6kmfld0d879i8nyuzb3h5lyg3p5p&rid=giphy.gif&ct=g"
+						alt=""
+					/>
+					<div class="text-center sm:text-left sm:flex-grow">
+						<div class="mb-4">
+							<p class="text-xl leading-tight">CAT</p>
+							<p class="text-sm leading-tight text-gray-400">You just got catjammed.</p>
+						</div>
+						<div>
+							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border-2 border-purple-400 text-purple-500 hover:bg-purple-600 hover:text-white">
+								All
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="sm:flex sm:items-center px-6 py-4">
+					<img
+						class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+						src="https://media2.giphy.com/media/jpbnoe3UIa8TU8LM13/giphy.gif?cid=ecf05e47yb6fo8f80hiw6kmfld0d879i8nyuzb3h5lyg3p5p&rid=giphy.gif&ct=g"
+						alt=""
+					/>
+					<div class="text-center sm:text-left sm:flex-grow">
+						<div class="mb-4">
+							<p class="text-xl leading-tight">JAM</p>
+							<p class="text-sm leading-tight text-gray-400">Vibe with me.</p>
+						</div>
+						<div>
+							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border-2 border-purple-400 text-purple-500 hover:bg-purple-600 hover:text-white">
+								Around
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="sm:flex sm:items-center px-6 py-4">
+					<img
+						class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+						src="https://media2.giphy.com/media/jpbnoe3UIa8TU8LM13/giphy.gif?cid=ecf05e47yb6fo8f80hiw6kmfld0d879i8nyuzb3h5lyg3p5p&rid=giphy.gif&ct=g"
+						alt=""
+					/>
+					<div class="text-center sm:text-left sm:flex-grow">
+						<div class="mb-4">
+							<p class="text-xl leading-tight">SURPRISE</p>
+							<p class="text-sm leading-tight text-gray-400">Bet ya didn't expect that.</p>
+						</div>
+						<div>
+							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border-2 border-purple-400 text-purple-500 hover:bg-purple-600 hover:text-white">
+								Vibes
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="sm:flex sm:items-center px-6 py-4">
+					<img
+						class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+						src="https://media2.giphy.com/media/jpbnoe3UIa8TU8LM13/giphy.gif?cid=ecf05e47yb6fo8f80hiw6kmfld0d879i8nyuzb3h5lyg3p5p&rid=giphy.gif&ct=g"
+						alt=""
+					/>
+					<div class="text-center sm:text-left sm:flex-grow">
+						<div class="mb-4">
+							<p class="text-xl leading-tight">CAT</p>
+							<p class="text-sm leading-tight text-gray-400">You just got catjammed.</p>
+						</div>
+						<div>
+							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border-2 border-purple-400 text-purple-500 hover:bg-purple-600 hover:text-white">
+								All
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="sm:flex sm:items-center px-6 py-4">
+					<img
+						class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+						src="https://media2.giphy.com/media/jpbnoe3UIa8TU8LM13/giphy.gif?cid=ecf05e47yb6fo8f80hiw6kmfld0d879i8nyuzb3h5lyg3p5p&rid=giphy.gif&ct=g"
+						alt=""
+					/>
+					<div class="text-center sm:text-left sm:flex-grow">
+						<div class="mb-4">
+							<p class="text-xl leading-tight">JAM</p>
+							<p class="text-sm leading-tight text-gray-400">Vibe with me.</p>
+						</div>
+						<div>
+							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border-2 border-purple-400 text-purple-500 hover:bg-purple-600 hover:text-white">
+								Around
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Construct;
