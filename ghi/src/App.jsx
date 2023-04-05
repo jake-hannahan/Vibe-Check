ac6fce177373bf81503eaacc8eb122adce03c9bf
Shaun <sragasa97@gmail.com>
2023-04-05 18:37:02 -0400

    Shaun: Added 4/5 journal

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -1,18 +1,16 @@
-
 import "./App.css";
 import Login from "./components/auth/Login";
 import Logout from "./components/auth/Logout";
 import Signup from "./components/auth/Signup";
 
 function App() {
-
 	return (
-    <div>
-      <Login />
-      <Logout />
-      <Signup />
-    </div>
-  );
+		<div>
+			<Login />
+			<Logout />
+			<Signup />
+		</div>
+	);
 }
 
 export default App;
