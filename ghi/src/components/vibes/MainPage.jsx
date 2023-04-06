709af1b46f5575abc9678efc8c3cf47e6d239c7b
Jay Shastri <jayshastri20@gmail.com>
2023-04-06 18:08:08 -0400

    TEAM started creating the front-end main page

---
+++ ghi/src/components/vibes/MainPage.jsx
@@ -0,0 +1,49 @@
+function MainPage() {
+    
+    
+    return (
+    <div className="container text-center mx-auto my-auto">
+      <h1 className="">Find a Vibe</h1>
+      <div className="container grid gap-2 md:grid-cols-3 xs:grid-cols-1 sm:grid-cols-2">
+            <div>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full">Productive</button>
+            </div>
+            <div>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full">Adventurous</button>
+            </div>
+            <div>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full">Confident</button>
+            </div>
+             <div>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full">Romantic</button>
+            </div>
+            <div>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full">Energetic</button>
+            </div>
+            <div>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full">Destructive</button>
+            </div>
+             <div>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full">Gloomy</button>
+            </div>
+            <div>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full">Rejected</button>
+            </div>
+            <div>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full">Melancholic</button>
+            </div>
+            <div>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full">Chill</button>
+            </div>
+            <div>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full">Lazy</button>
+            </div>
+            <div>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full">Dreamy</button>
+            </div>
+      </div>
+    </div>
+    )
+};
+
+export default MainPage;
