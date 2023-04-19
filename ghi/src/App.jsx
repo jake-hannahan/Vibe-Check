66f22e77a9e5aef2b50a432cac70b0df47c1c3c5
Shaun <sragasa97@gmail.com>
2023-04-19 11:56:47 -0400

    Added placeholder comments in app.jsx

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -7,19 +7,25 @@ import Nav from "./components/Nav";
 import MainPage from "./components/vibes/MainPage";
 import SongList from "./components/cards/SongList";
 
-
 function App() {
 	return (
 		<BrowserRouter>
 			<Nav />
 			<Routes>
-				<Route path="/" element={<MainPage />}/>
+				<Route path="/" element={<MainPage />} />
 				<Route path="account">
-					<Route path="login" element={<Login />}/>
-					<Route path="signup" element={<Signup />}/>
-					<Route path="logout" element={<Logout />}/>
+					<Route path="login" element={<Login />} />
+					<Route path="signup" element={<Signup />} />
+					<Route path="logout" element={<Logout />} />
 				</Route>
-				<Route path="song" element={<SongList playlistId="643ecf6a5bbf3e1d03d6ac0a" spotifyId="6dl39Fce97aewH2tI2Rlvi"/>}/>
+				<Route
+					path="song"
+					element={<SongList playlistId="643ecf6a5bbf3e1d03d6ac0a" spotifyId="6dl39Fce97aewH2tI2Rlvi" />}
+				/>
+				{/* Jake */}
+				{/* Shaun */}
+				{/* Alana */}
+				{/* Jay */}
 			</Routes>
 		</BrowserRouter>
 	);
