4094f0f85b8e3b49a09560a9b7416abcd927597c
Shaun <sragasa97@gmail.com>
2023-04-19 12:52:59 -0400

    Added vibeAPI reducer and middleware to store

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -6,6 +6,7 @@ import Signup from "./components/auth/Signup";
 import Nav from "./components/Nav";
 import MainPage from "./components/vibes/MainPage";
 import SongList from "./components/cards/SongList";
+import VibeDetailPage from "./components/vibes/VibeDetailPage";
 
 function App() {
 	return (
@@ -23,7 +24,7 @@ function App() {
 					element={<SongList playlistId="643ecf6a5bbf3e1d03d6ac0a" spotifyId="6dl39Fce97aewH2tI2Rlvi" />}
 				/>
 				{/* Jake */}
-				{/* Shaun */}
+				<Route path="detail" element={<VibeDetailPage vibeId="64401b1e741a5c8cf7a908db" />} />
 				{/* Alana */}
 				{/* Jay */}
 			</Routes>
--- ghi/src/app/store.js
+++ ghi/src/app/store.js
@@ -1,20 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit'
-import loginReducer from '../features/auth/loginSlice'
-import { setupListeners } from '@reduxjs/toolkit/query'
-import { authApi } from '../services/auth';
+import { configureStore } from "@reduxjs/toolkit";
+import loginReducer from "../features/auth/loginSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { authApi } from "../services/auth";
 import signupReducer from "../features/auth/signupSlice";
-import { spotifyApi } from '../services/spotify';
-
+import { spotifyApi } from "../services/spotify";
+import { vibeApi } from "../services/vibes";
 
 export const store = configureStore({
-  reducer: {
-    login: loginReducer,
-    signup: signupReducer,
-    [authApi.reducerPath]: authApi.reducer,
-    [spotifyApi.reducerPath]: spotifyApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([authApi.middleware, spotifyApi.middleware]),
+	reducer: {
+		login: loginReducer,
+		signup: signupReducer,
+		[authApi.reducerPath]: authApi.reducer,
+		[spotifyApi.reducerPath]: spotifyApi.reducer,
+		[vibeApi.reducerPath]: vibeApi.reducer,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat([authApi.middleware, spotifyApi.middleware, vibeApi.middleware]),
 });
 
-setupListeners(store.dispatch)
+setupListeners(store.dispatch);
