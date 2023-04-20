e85d3778eb46bfdc19b4b5e5e29a46f5eb606005
Alana <alana.torrez@gmail.com>
2023-04-19 20:12:04 -0500

    Added to vibeslist

--- ghi/src/components/cards/ListCard.jsx
+++ ghi/src/components/cards/ListCard.jsx
@@ -1,148 +1,175 @@
+// import React from "react";
 
+import { useGetVibesQuery } from "../../services/vibes";
 
 function ListCard(props) {
-    return (
-      <>
-        <div className="grid grid-cols-4 grid-rows-2 gap-4">
-          <div className="max-w-xs rounded overflow-hidden shadow-lg p-8">
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Vibe Name</div>
-              <div className="font-bold text-xl mb-2">Creator</div>
-            </div>
-            <div className="w-32 h-32 mx-auto">
-              <img
-                className="w-full h-full"
-                src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmU0ZjdlNTExN2VmZTE0OGY0YzZjYWM5NzY3MmYxYjc4OTkwNjZhNSZjdD1n/jpbnoe3UIa8TU8LM13/giphy.gif"
-                alt="Sunset in the mountains"
-              />
-            </div>
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Playlist Name</div>
-              <div className="font-bold text-xl mb-2">Activities</div>
-            </div>
+  const { data, isLoading } = useGetVibesQuery;
+  if (isLoading) return <div>Loading...</div>;
+  return (
+    <>
+      <div className="grid grid-cols-4 grid-rows-2 gap-4">
+        <div className="max-w-xs rounded overflow-hidden shadow-lg p-8">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Vibe Name</div>
+            <div className="font-bold text-xl mb-2">Creator</div>
           </div>
-          <div className="max-w-xs rounded overflow-hidden shadow-lg p-8">
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Vibe Name</div>
-              <div className="font-bold text-xl mb-2">Creator</div>
-            </div>
-            <div className="w-32 h-32 mx-auto">
-              <img
-                className="w-full h-full"
-                src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmU0ZjdlNTExN2VmZTE0OGY0YzZjYWM5NzY3MmYxYjc4OTkwNjZhNSZjdD1n/jpbnoe3UIa8TU8LM13/giphy.gif"
-                alt="Sunset in the mountains"
-              />
-            </div>
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Playlist Name</div>
-              <div className="font-bold text-xl mb-2">Activities</div>
-            </div>
+          <div className="w-32 h-32 mx-auto">
+            <img
+              className="w-full h-full"
+              src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmU0ZjdlNTExN2VmZTE0OGY0YzZjYWM5NzY3MmYxYjc4OTkwNjZhNSZjdD1n/jpbnoe3UIa8TU8LM13/giphy.gif"
+              alt="Sunset in the mountains"
+            />
           </div>
-          <div className="max-w-xs rounded overflow-hidden shadow-lg p-8">
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Vibe Name</div>
-              <div className="font-bold text-xl mb-2">Creator</div>
-            </div>
-            <div className="w-32 h-32 mx-auto">
-              <img
-                className="w-full h-full"
-                src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmU0ZjdlNTExN2VmZTE0OGY0YzZjYWM5NzY3MmYxYjc4OTkwNjZhNSZjdD1n/jpbnoe3UIa8TU8LM13/giphy.gif"
-                alt="Sunset in the mountains"
-              />
-            </div>
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Playlist Name</div>
-              <div className="font-bold text-xl mb-2">Activities</div>
-            </div>
-          </div>
-          <div className="max-w-xs rounded overflow-hidden shadow-lg p-8">
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Vibe Name</div>
-              <div className="font-bold text-xl mb-2">Creator</div>
-            </div>
-            <div className="w-32 h-32 mx-auto">
-              <img
-                className="w-full h-full"
-                src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmU0ZjdlNTExN2VmZTE0OGY0YzZjYWM5NzY3MmYxYjc4OTkwNjZhNSZjdD1n/jpbnoe3UIa8TU8LM13/giphy.gif"
-                alt="Sunset in the mountains"
-              />
-            </div>
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Playlist Name</div>
-              <div className="font-bold text-xl mb-2">Activities</div>
-            </div>
-          </div>
-          <div className="max-w-xs rounded overflow-hidden shadow-lg p-8">
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Vibe Name</div>
-              <div className="font-bold text-xl mb-2">Creator</div>
-            </div>
-            <div className="w-32 h-32 mx-auto">
-              <img
-                className="w-full h-full"
-                src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmU0ZjdlNTExN2VmZTE0OGY0YzZjYWM5NzY3MmYxYjc4OTkwNjZhNSZjdD1n/jpbnoe3UIa8TU8LM13/giphy.gif"
-                alt="Sunset in the mountains"
-              />
-            </div>
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Playlist Name</div>
-              <div className="font-bold text-xl mb-2">Activities</div>
-            </div>
-          </div>
-          <div className="max-w-xs rounded overflow-hidden shadow-lg p-8">
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Vibe Name</div>
-              <div className="font-bold text-xl mb-2">Creator</div>
-            </div>
-            <div className="w-32 h-32 mx-auto">
-              <img
-                className="w-full h-full"
-                src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmU0ZjdlNTExN2VmZTE0OGY0YzZjYWM5NzY3MmYxYjc4OTkwNjZhNSZjdD1n/jpbnoe3UIa8TU8LM13/giphy.gif"
-                alt="Sunset in the mountains"
-              />
-            </div>
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Playlist Name</div>
-              <div className="font-bold text-xl mb-2">Activities</div>
-            </div>
-          </div>
-          <div className="max-w-xs rounded overflow-hidden shadow-lg p-8">
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Vibe Name</div>
-              <div className="font-bold text-xl mb-2">Creator</div>
-            </div>
-            <div className="w-32 h-32 mx-auto">
-              <img
-                className="w-full h-full"
-                src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmU0ZjdlNTExN2VmZTE0OGY0YzZjYWM5NzY3MmYxYjc4OTkwNjZhNSZjdD1n/jpbnoe3UIa8TU8LM13/giphy.gif"
-                alt="Sunset in the mountains"
-              />
-            </div>
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Playlist Name</div>
-              <div className="font-bold text-xl mb-2">Activities</div>
-            </div>
-          </div>
-          <div className="max-w-xs rounded overflow-hidden shadow-lg p-8">
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Vibe Name</div>
-              <div className="font-bold text-xl mb-2">Creator</div>
-            </div>
-            <div className="w-32 h-32 mx-auto">
-              <img
-                className="w-full h-full"
-                src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmU0ZjdlNTExN2VmZTE0OGY0YzZjYWM5NzY3MmYxYjc4OTkwNjZhNSZjdD1n/jpbnoe3UIa8TU8LM13/giphy.gif"
-                alt="Sunset in the mountains"
-              />
-            </div>
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Playlist Name</div>
-              <div className="font-bold text-xl mb-2">Activities</div>
-            </div>
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Playlist Name</div>
+            <div className="font-bold text-xl mb-2">Activities</div>
           </div>
         </div>
-      </>
-    );
-    }
+        <div className="max-w-xs rounded overflow-hidden shadow-lg p-8">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Vibe Name</div>
+            <div className="font-bold text-xl mb-2">Creator</div>
+          </div>
+          <div className="w-32 h-32 mx-auto">
+            <img
+              className="w-full h-full"
+              src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmU0ZjdlNTExN2VmZTE0OGY0YzZjYWM5NzY3MmYxYjc4OTkwNjZhNSZjdD1n/jpbnoe3UIa8TU8LM13/giphy.gif"
+              alt="Sunset in the mountains"
+            />
+          </div>
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Playlist Name</div>
+            <div className="font-bold text-xl mb-2">Activities</div>
+          </div>
+        </div>
+        <div className="max-w-xs rounded overflow-hidden shadow-lg p-8">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Vibe Name</div>
+            <div className="font-bold text-xl mb-2">Creator</div>
+          </div>
+          <div className="w-32 h-32 mx-auto">
+            <img
+              className="w-full h-full"
+              src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmU0ZjdlNTExN2VmZTE0OGY0YzZjYWM5NzY3MmYxYjc4OTkwNjZhNSZjdD1n/jpbnoe3UIa8TU8LM13/giphy.gif"
+              alt="Sunset in the mountains"
+            />
+          </div>
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Playlist Name</div>
+            <div className="font-bold text-xl mb-2">Activities</div>
+          </div>
+        </div>
+        <div className="max-w-xs rounded overflow-hidden shadow-lg p-8">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Vibe Name</div>
+            <div className="font-bold text-xl mb-2">Creator</div>
+          </div>
+          <div className="w-32 h-32 mx-auto">
+            <img
+              className="w-full h-full"
+              src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmU0ZjdlNTExN2VmZTE0OGY0YzZjYWM5NzY3MmYxYjc4OTkwNjZhNSZjdD1n/jpbnoe3UIa8TU8LM13/giphy.gif"
+              alt="Sunset in the mountains"
+            />
+          </div>
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Playlist Name</div>
+            <div className="font-bold text-xl mb-2">Activities</div>
+          </div>
+        </div>
+        <div className="max-w-xs rounded overflow-hidden shadow-lg p-8">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Vibe Name</div>
+            <div className="font-bold text-xl mb-2">Creator</div>
+          </div>
+          <div className="w-32 h-32 mx-auto">
+            <img
+              className="w-full h-full"
+              src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmU0ZjdlNTExN2VmZTE0OGY0YzZjYWM5NzY3MmYxYjc4OTkwNjZhNSZjdD1n/jpbnoe3UIa8TU8LM13/giphy.gif"
+              alt="Sunset in the mountains"
+            />
+          </div>
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Playlist Name</div>
+            <div className="font-bold text-xl mb-2">Activities</div>
+          </div>
+        </div>
+        <div className="max-w-xs rounded overflow-hidden shadow-lg p-8">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Vibe Name</div>
+            <div className="font-bold text-xl mb-2">Creator</div>
+          </div>
+          <div className="w-32 h-32 mx-auto">
+            <img
+              className="w-full h-full"
+              src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmU0ZjdlNTExN2VmZTE0OGY0YzZjYWM5NzY3MmYxYjc4OTkwNjZhNSZjdD1n/jpbnoe3UIa8TU8LM13/giphy.gif"
+              alt="Sunset in the mountains"
+            />
+          </div>
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Playlist Name</div>
+            <div className="font-bold text-xl mb-2">Activities</div>
+          </div>
+        </div>
+        <div className="max-w-xs rounded overflow-hidden shadow-lg p-8">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Vibe Name</div>
+            <div className="font-bold text-xl mb-2">Creator</div>
+          </div>
+          <div className="w-32 h-32 mx-auto">
+            <img
+              className="w-full h-full"
+              src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmU0ZjdlNTExN2VmZTE0OGY0YzZjYWM5NzY3MmYxYjc4OTkwNjZhNSZjdD1n/jpbnoe3UIa8TU8LM13/giphy.gif"
+              alt="Sunset in the mountains"
+            />
+          </div>
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Playlist Name</div>
+            <div className="font-bold text-xl mb-2">Activities</div>
+          </div>
+        </div>
+        <div className="max-w-xs rounded overflow-hidden shadow-lg p-8">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Vibe Name</div>
+            <div className="font-bold text-xl mb-2">Creator</div>
+          </div>
+          <div className="w-32 h-32 mx-auto">
+            <img
+              className="w-full h-full"
+              src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmU0ZjdlNTExN2VmZTE0OGY0YzZjYWM5NzY3MmYxYjc4OTkwNjZhNSZjdD1n/jpbnoe3UIa8TU8LM13/giphy.gif"
+              alt="Sunset in the mountains"
+            />
+          </div>
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Playlist Name</div>
+            <div className="font-bold text-xl mb-2">Activities</div>
+          </div>
+        </div>
+
+        {data.vibes.map((vibe) => (
+          <div
+            className="max-w-xs rounded overflow-hidden shadow-lg p-8"
+            key={vibe.vibe}
+          >
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{vibe.name}</div>
+              <div className="font-bold text-xl mb-2">{vibe.created_by}</div>
+            </div>
+            <div className="w-32 h-32 mx-auto">
+              <img
+                className="w-full h-full"
+                src={vibe.picture_url}
+                alt="Sunset in the mountains"
+              />
+            </div>
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{vibe.playlist_id}</div>
+              <div className="font-bold text-xl mb-2">{vibe.activites}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
 
 export default ListCard;
--- ghi/src/components/vibes/VibesListPage.jsx
+++ ghi/src/components/vibes/VibesListPage.jsx
@@ -2,9 +2,6 @@ import ListCard from "../cards/ListCard";
 
 function VibesListPage() {
     return (
-      // navbar auto added in app.jsx
-
-      // needs header type to introduce page purpose
       // needs text that shows applied filter type
       // but default filter is most recent vibes
 
@@ -14,17 +11,45 @@ function VibesListPage() {
 
       // needs scrollbar
 
-      <div class="p-5 bg-black">
-        <div class="grid grid-cols-6 gap-4 grid-flow-row grid-">
-          <div class="p-5 text-center bg-white row-start-1 row-end-3">A</div>
-          <div class="p-5 text-center bg-white col-start-2 col-end-5 font-extrabold">
+      <div className="p-5 bg-black">
+        <div className="grid grid-cols-6 gap-4 grid-flow-row grid-">
+          <div className="p-5 text-center bg-white row-start-1 row-end-3">
+            <div className="relative w-full lg:max-w-sm">
+              <select className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
+                <option>Pick a Mood</option>
+                <option>Adventurous</option>
+                <option>Chill</option>
+                <option>Confident</option>
+                <option>Destructive</option>
+                <option>Dreamy</option>
+                <option>Energetic</option>
+                <option>Gloomy</option>
+                <option>Lazy</option>
+                <option>Melancholic</option>
+                <option>Productive</option>
+                <option>Rejected</option>
+                <option>Romantic</option>
+              </select>
+              <div>
+                <h2 className="mb-2 text-lg text-left font-semibold text-gray-900 dark:text-black pt-12">
+                  Recent ~moods~
+                </h2>
+                <ul className="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400 text-left">
+                  <li>Chill</li>
+                  <li>Dreamy</li>
+                  <li>Lazy</li>
+                  <li>Gloomy</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="p-5 text-center bg-white col-start-2 col-end-5 font-extrabold">
             VIBES IN YOUR FACE
           </div>
-          <div class="p-5 text-center bg-white col-start-5 col-end-7">
-            Current schmood
+          <div className="p-5 text-center bg-white col-start-5 col-end-7">
+            Current schmood!!!
           </div>
-          <div class="p-5 text-center bg-white col-start-2 col-end-7 row-start-2 row-end-auto">
-            E
+          <div className="p-5 text-center bg-white col-start-2 col-end-7 row-start-2 row-end-auto">
             <ListCard />
           </div>
         </div>
