dcaf548b71e8c49690447e5eb48815e214828d2b
Jay Shastri <jayshastri20@gmail.com>
2023-04-24 22:01:33 -0400

    JAY: Designing the MyVibesPage and Card

--- ghi/src/components/cards/MyVibeCard.jsx
+++ ghi/src/components/cards/MyVibeCard.jsx
@@ -8,21 +8,38 @@ function MyVibesCard(props) {
         return str.split(" ").map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" ").replace(/_/g, " ");
     };
 
+    const moodColorMap = {
+        chill: 'bg-sky-400',
+        lazy: 'bg-teal-400',
+        dreamy: 'bg-cyan-400',
+        productive: 'bg-orange-400',
+        adventurous: 'bg-yellow-400',
+        confident: 'bg-amber-400',
+        romantic: 'bg-rose-400',
+        energetic: 'bg-lime-400',
+        destructive: 'bg-stone-400',
+        gloomy: 'bg-gray-400',
+        rejected: 'bg-violet-400',
+        melancholic: 'bg-indigo-400',
+    };
+
+    const moodColor = moodColorMap[props.vibe.mood.toLowerCase()]||"bg-gray-400";
+
     return (
     <>
-        <div className="flex justify-center bg-black text-white mx-auto p-4 box-content h-96 max-w-8xl overscroll-contain rounded-lg border-x-4 border-y-4 border-green-500">
+        <div className={`flex justify-center bg-black text-white mx-auto p-4 box-content h-96 max-w-8xl overscroll-contain rounded-lg ${moodColor}`}>
                 <div className="flex grid grid-cols-2 w-full h-full" key={props.vibe.id}>
                     {/* Left side of the card */}
-                    <div className="relative border-2 border-sky-500 flex-1 p-2 w-full font-apple-system text-lg font-bold text-justify text-green-500">{capitalize(props.vibe.name)}
+                    <div className="relative flex-1 p-2 w-full font-apple-system text-lg font-bold text-justify text-black">{capitalize(props.vibe.name)}
                         {/* Mood box */}
                         <div 
-                            className="static left-0 border-2 border-red-300 flex-1 p-2 w-full text-base font-apple-system text-white text-center">{capitalize(props.vibe.mood)}
+                            className="static left-0 flex-1 p-2 w-full text-base font-apple-system text-black text-center">{capitalize(props.vibe.mood)}
                         </div> 
                             <ul>
                                 <li>
                                     {props.vibe.activities.map((activity) => (
                                         // activities box
-                                        <div className="static right-10 left-10 border-2 border-white flex-1 text-base font-apple-system text-white text-center" key={activity.name}>
+                                        <div className="static right-10 left-10 flex-1 text-base font-apple-system text-black text-center" key={activity.name}>
                                             <li className="p-2 max-w-prose">{capitalize(activity.category)}</li>
                                             <li className="p-2 max-w-prose">{capitalize(activity.name)}</li>                                           
                                         </div>
@@ -30,14 +47,14 @@ function MyVibesCard(props) {
                                 </li>
                             </ul>
                             {/* Buttons box */}
-                            <ul className="absolute bottom-0 left-3 border-2 border-orange-200 flex justify-center mt-auto gap-x-6">
+                            <ul className="absolute bottom-0 left-3 flex justify-center mt-auto gap-x-6">
                                 <li>
-                                    <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                                    <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-lg">
                                         <NavLink to={{pathname:'/edit'}} state={{vibe: props.vibe}}>Edit</NavLink>
                                     </button>
                                 </li>
                                 <li>
-                                    <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                                    <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-lg">
                                         <NavLink to={{pathname: '/detail'}} state={{vibeId: props.vibe.id}}>Details</NavLink>
                                     </button>
                                 </li>
