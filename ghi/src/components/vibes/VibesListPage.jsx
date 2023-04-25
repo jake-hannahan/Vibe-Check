11e5d7cddb50dcd10834dd81ec9b5dfd053ffb6b
Alana <alana.torrez@gmail.com>
2023-04-25 10:50:11 -0500

    Added some to vibes list formatting

--- ghi/src/components/vibes/VibesListPage.jsx
+++ ghi/src/components/vibes/VibesListPage.jsx
@@ -5,18 +5,19 @@ import { useGetVibesQuery } from "../../services/vibes";
 
 function VibesListPage() {
     const moods = [
-      {id: 1, name: 'Adventurous'},
-      {id: 2, name: 'Chill'},
-      {id: 3, name: 'Confident'},
-      {id: 4, name: 'Destructive'},
-      {id: 5, name: 'Dreamy'},
-      {id: 6, name: 'Energetic'},
-      {id: 7, name: 'Gloomy'},
-      {id: 8, name: 'Lazy'},
-      {id: 9, name: 'Melancholic'},
-      {id: 10, name: 'Productive'},
-      {id: 11, name: 'Rejected'},
-      {id: 12, name: 'Romantic'},
+      { id: 1, name: "All" },
+      { id: 2, name: "Adventurous" },
+      { id: 3, name: "Chill" },
+      { id: 4, name: "Confident" },
+      { id: 5, name: "Destructive" },
+      { id: 6, name: "Dreamy" },
+      { id: 7, name: "Energetic" },
+      { id: 8, name: "Gloomy" },
+      { id: 9, name: "Lazy" },
+      { id: 10, name: "Melancholic" },
+      { id: 11, name: "Productive" },
+      { id: 12, name: "Rejected" },
+      { id: 13, name: "Romantic" },
     ];
 
     const { data, isLoading, isSuccess, isError, error } = useGetVibesQuery({
@@ -28,17 +29,24 @@ function VibesListPage() {
 // declaring a variable to be used later--instantiate or whatever, not sure exactly what is happening
     // let currentMood;
 
-    const[selectedMood, setSelectedMood] = useState('chill')
+    const[selectedMood, setSelectedMood] = useState('All')
     // sets initial state of data to load vibes with chill mood
         // const[changeColor, setChangeColor] = useState(false)
-    const[moodDisplay, setMoodDisplay] = useState('chill')
+    // const[moodDisplay, setMoodDisplay] = useState('chill')
 
-    const filteredMood = () => {
-
-    }
     const filteredData = () => {
-    return data.filter((vibe) => vibe.mood.toLowerCase().includes(selectedMood.toLowerCase()));
-  };
+    if (selectedMood === "All") {
+    return data
+  }
+    else {
+      return data.filter((vibe) =>
+      vibe.mood.toLowerCase().includes(selectedMood.toLowerCase())
+      );
+  }
+};
+
+
+
 
     if (isLoading) {
       vibesData = (
@@ -71,7 +79,7 @@ function VibesListPage() {
 
       <div className="p-5 bg-black">
         <div className="grid grid-cols-6 gap-4 grid-flow-row grid-">
-          <div className="p-5 text-center bg-white row-start-1 row-end-3">
+          <div className="p-5 text-center bg-white row-start-1 row-end-1 col-start-5 col-end-5">
             <div className="relative w-full lg:max-w-sm">
               <select
                 onChange={(e) => setSelectedMood(e.target.value)}
@@ -85,32 +93,29 @@ function VibesListPage() {
                   </option>
                 ))}
               </select>
-              <div>
-                <h2 className="truncate md:text-clip mb-2 text-lg text-left font-semibold text-gray-900 dark:text-black pt-12">
-                  Recent ~moods~
-                </h2>
-                <ul className="truncate md:text-clip max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400 text-left">
-                  <li>Chill</li>
-                  <li>Dreamy</li>
-                  <li>Lazy</li>
-                  <li>Gloomy</li>
-                </ul>
-              </div>
             </div>
           </div>
-          <div className="p-5 text-center bg-white col-start-2 col-end-5 font-extrabold">
-            VIBES IN YOUR FACE
+          <div className="p-5 text-center bg-white col-start-1 col-end-5 font-extrabold">
+            VIBES FOR LIFE
           </div>
-          <div className="p-5 text-center bg-white col-start-5 col-end-7">
-            {selectedMood}
+          <div className="p-5 text-center bg-white col-start-6 col-end-7">
+            Current Mood: {selectedMood}
           </div>
           <div
             // onChange={handleChange}
-            className="p-5 text-center bg-white col-start-2 col-end-7 row-start-2 row-end-auto"
+            className="p-5 text-center bg-white col-start-1 col-end-7 row-start-2 row-auto col-auto"
           >
-            {vibesData}
-            {/* <ListCard /> */}
+            <div className="flex flex-row ">
+              <div className="flex flex-wrap -mx-1">
+                {/* <div className="w-2/5 p-2"></div> */}
+                {/* <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3"> */}
+
+                {vibesData}
+                {/* <ListCard /> */}
+              </div>
+            </div>
           </div>
+        {/* </div> */}
         </div>
       </div>
     );
